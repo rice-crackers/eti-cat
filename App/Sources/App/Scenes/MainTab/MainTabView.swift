bc1fd7811131eb02b2d76fa2ac77269f0c0d5308
baegteun <baegteun@gmail.com>
2023-01-14 21:15:44 +0900

    :sparkles: :: MainTabView

---
+++ App/Sources/App/Scenes/MainTab/MainTabView.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabView.swift
+//  Eticat
+//
+//  Created by 최형우 on 2023/01/14.
+//  Copyright © 2023 com.rice. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
